d2db1fa12689a596604cff0bd3f22ad1af0683ca
Sidney Sousa <sidney.sousa@ifms.edu.br>
2025-05-08 09:54:09 -0400

    commit inicial

---
+++ src/Estatistica.ts
@@ -0,0 +1,31 @@
+export class Estatistica {
+  private _numeros: number[];
+
+  constructor(numeros: number[]) {
+    this._numeros = numeros;
+  }
+
+  media() {
+    return -1;
+  }
+
+  mediana() {
+    return -1;
+  }
+
+  moda() {
+    return -1;
+  }
+
+  variancia() {
+    return -1;
+  }
+
+  desvioPadrao() {
+    return -1;
+  }
+
+  coeficienteDeVariacao() {
+    return -1;
+  }
+}
